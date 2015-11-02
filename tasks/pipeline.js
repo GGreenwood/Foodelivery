f2517435428bfe0b7f5e352ffdd0e7977aca7804
AbsoluteBlack <achievingescapevelocity@gmail.com>
2015-11-02 05:10:42 +0000

    added slick to grunt pipeline

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -16,7 +16,9 @@ var tmpPath = '.tmp/public/';
 // (if you're using LESS with the built-in default config, you'll want
 //  to change `assets/styles/importer.less` instead.)
 var cssFilesToInject = [
-  'styles/**/*.css'
+  'styles/**/*.css',
+  'slick/**/*.css'
+
 ];
 
 
@@ -33,7 +35,7 @@ var jsFilesToInject = [
   // All of the rest of your client-side js files
   // will be injected here in no particular order.
   'js/**/*.js',
-
+  'slick/**/*.js'
   // Use the "exclude" operator to ignore files
   // '!js/ignore/these/files/*.js'
 ];
