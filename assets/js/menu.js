e3f412ae2da268766274af5690111cfe5dfc3f3e
Garrett Greenwood <garrettagreenwood@gmail.com>
2015-11-16 05:59:13 +0000

    commit
    Signed-off-by: Garrett Greenwood <garrettagreenwood@gmail.com>

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,2 +1,22 @@
 $(document).ready(function() {
+    id = getId();
+    io.socket.get("/restaurant/" + id, function(data) {
+        console.log(data);
+        newItem(data.items[0]);
+    });
+
+    $('.foodcarousel').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
 });
+
+function newItem(item) {
+    mainDiv = document.createElement('div');
+    $(mainDiv).addClass("row center");
+    console.log(mainDiv);
+}
--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,6 +1,14 @@
 var id;
 $(document).ready(function() {
-    var parameters = document.URL.split('/');
-    id = parameters[parameters.length-1];
-    $("#cart-button")[0].href = "/cart/" + id;
+    $("#cart-button")[0].href = "/cart/" + getId();
 });
+
+function getId() {
+    if(id) {
+        return id;
+    } else {
+        var parameters = document.URL.split('/');
+        id = parameters[parameters.length-1];
+        return id;
+    }
+}
