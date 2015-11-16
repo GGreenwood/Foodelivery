5041c3e1d5f3e9d51ce8ee0542f7597276e79477
Garrett Greenwood <garrettagreenwood@gmail.com>
2015-11-16 01:55:54 +0000

    Added star ratings to review page
    Signed-off-by: Garrett Greenwood <garrettagreenwood@gmail.com>

--- assets/js/reviews.js
+++ assets/js/reviews.js
@@ -1,7 +1,38 @@
 function submit() {
     text = $("#text").val();
-    io.socket.post("/review", {review: text}, function(response) {
+    data = {
+        review: text,
+        score: rating
+    };
+
+    io.socket.post("/review", data, function(response) {
         console.log(response);
         window.location = "reviewlist";
     });
+}
+
+rating = 0;
+
+function displayUpTo(rating) {
+    divs = $(".star").splice(0, rating);
+    $.each(divs, function(index, element) {
+        element.innerHTML = "star_rate";
+    });
+    divs = $(".star").splice(rating, 5);
+    $.each(divs, function(index, element) {
+        element.innerHTML = "stars";
+    });
+}
+
+function showStar(div) {
+    star = parseInt(div.getAttribute("star"));
+    displayUpTo(star);
+}
+
+function setStar(div) {
+    rating = parseInt(div.getAttribute("star"));
+}
+
+function resetStar(div) {
+    displayUpTo(rating);
 }
