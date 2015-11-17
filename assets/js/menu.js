56fc9189b2fc7441896f00a54e2e683b29b62c3e
Garrett Greenwood <garrettagreenwood@gmail.com>
2015-11-17 19:08:00 +0000

    Added code for item click increment
    Signed-off-by: Garrett Greenwood <garrettagreenwood@gmail.com>

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -34,9 +34,12 @@ $(document).ready(function() {
 function newItem(item) {
     return '<div class="row">' + 
         '<div class="col s6 center">' + 
-        '<a onclick="addItem(' + item.id + ')" class="waves-effect waves-light btn-large">' + item.name + '</a>' + 
+        '<a onclick="addItem(' + item.id + ')" class="waves-effect waves-light btn-large" style="width:100%;">' + item.name + '</a>' + 
         '</div>' +
         '<div class="col s3"><h4>' + formatPrice(item.price) + '</h4></div>' + 
+        '<div class="col s3 input-field">' + 
+        '<input type="number" id="item' + item.id + '" class="materialize-textarea"></input>' +
+        '</div>' +
         '</div>';
 }
 
@@ -48,4 +51,11 @@ function newImage(item) {
 
 function formatPrice(price) {
     return '$' + parseInt(price).toFixed(2);
+}
+
+function addItem(id) {
+    input = $("#item" + id);
+    input.val(function(i, oldval) {
+        return ++oldval;
+    });
 }
